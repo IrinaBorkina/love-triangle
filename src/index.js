8ed47664e41128bb123a8c1e2bd9eafc21d4d7c5
IrinaBorkina <44750544+IrinaBorkina@users.noreply.github.com>
2019-02-20 08:42:58 +0300

    solution love-triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  k = 0;
+  for (var n = 0; n < preferences.length; n++) {
+    Sam = preferences[n];
+    Mary = preferences[Sam - 1];
+    Alex = preferences[Mary - 1];
+    
+    if (Alex - 1 === n && Sam < Mary && Mary > Alex) {
+        k = k + 1;
+    }
+  }
+  return k;
 };
