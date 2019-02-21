0cf266102b23c677aaeef070bb70706100ed1d62
IrinaBorkina <44750544+IrinaBorkina@users.noreply.github.com>
2019-02-21 13:15:33 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  k = 0;
-  for (var n = 0; n < preferences.length; n++) {
-    Sam = preferences[n];
-    Mary = preferences[Sam - 1];
-    Alex = preferences[Mary - 1];
-    
-    if (Alex - 1 === n && Sam < Mary && Mary > Alex) {
-        k = k + 1;
+  let k = 0;
+  for (let n = 0; n < preferences.length; n++) {
+    let next1 = preferences[n] - 1;
+    if (next1 == n) continue;
+    let next2 = preferences[next1] - 1;
+    if (next1 == next2) continue;
+    let next3 = preferences[next2] - 1;
+    if (next3 == next2) continue;
+    if (next3 === n) {
+        k++;
     }
   }
-  return k;
+  return k / 3;
 };
